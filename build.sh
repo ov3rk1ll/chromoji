b77ddbf942926a126e42d210c11b95ca561a9ba0
Martin Smeeckaert <smeeckaert@novius.fr>
2015-11-24 09:04:37 +0100

    build updated

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-google-chrome --pack-extension=/git/chromoji/chromoji --pack-extension-key=/git/chromoji/chomoji.pem
+google-chrome --pack-extension=/git/chromoji/chromoji --pack-extension-key=/git/chromoji/chromoji.pem
