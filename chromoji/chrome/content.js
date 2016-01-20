7a7ae8bacb08b5ac7c4f82f49f6160b1d218acd4
Martin Smeeckaert <smeeckaert@novius.fr>
2016-01-20 09:02:18 +0100

    optimize performance

--- chromoji/chrome/content.js
+++ chromoji/chrome/content.js
@@ -3,9 +3,9 @@ url = chrome.extension.getURL("data/images/emoji/");
 emoji.img_path = url;
 
 emoji.img_sets = {
-    'apple'   : {'path': url + 'emoji-data/img-apple-64/', 'sheet': '/emoji-data/sheet_apple_64.png', 'mask': 1 },
-    'google'  : {'path': url + 'emoji-data/img-google-64/', 'sheet': '/emoji-data/sheet_google_64.png', 'mask': 2 },
-    'twitter' : {'path': url + 'emoji-data/img-twitter-72/', 'sheet': '/emoji-data/sheet_twitter_64.png', 'mask': 4 },
+    'apple': {'path': url + 'emoji-data/img-apple-64/', 'sheet': '/emoji-data/sheet_apple_64.png', 'mask': 1 },
+    'google': {'path': url + 'emoji-data/img-google-64/', 'sheet': '/emoji-data/sheet_google_64.png', 'mask': 2 },
+    'twitter': {'path': url + 'emoji-data/img-twitter-72/', 'sheet': '/emoji-data/sheet_twitter_64.png', 'mask': 4 },
     'emojione': {'path': url + 'emoji-data/img-emojione-64/', 'sheet': '/emoji-data/sheet_emojione_64.png', 'mask': 8 }
 };
 
@@ -23,19 +23,23 @@ storage.get('type', function (data) {
             return this.nodeType === 3;
         })
         .each(function () {
-            $this = $(this);
-            content = emoji.replace_unified(this.textContent);
+            var $this = $(this);
+            var self = this;
+            content = emoji.replace_unified(self.textContent);
+
             if (content != this.textContent) {
-                $parent = $this.parent();
-                fontSize = $parent.css('font-size');
-                var replacementNode = document.createElement('span');
-                replacementNode.className = 'emoji-container';
-                replacementNode.innerHTML = emoji.replace_unified(this.textContent);
-                this.parentNode.insertBefore(replacementNode, this);
-                this.parentNode.removeChild(this);
-                if (fontSize != '16px') {
-                    $parent.find('.emoji-sizer').css({width: fontSize, height: fontSize});
-                }
+                requestAnimationFrame(function () {
+                    $parent = $this.parent();
+                    fontSize = $parent.css('font-size');
+                    var replacementNode = document.createElement('span');
+                    replacementNode.className = 'emoji-container';
+                    replacementNode.innerHTML = emoji.replace_unified(self.textContent);
+                    self.parentNode.insertBefore(replacementNode, self);
+                    self.parentNode.removeChild(self);
+                    if (fontSize != '16px') {
+                        $parent.find('.emoji-sizer').css({width: fontSize, height: fontSize});
+                    }
+                });
             }
 
         });
