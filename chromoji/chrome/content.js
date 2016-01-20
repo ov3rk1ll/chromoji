69846267b66214047cc673eada142e82f0486600
Martin Smeeckaert <smeeckaert@novius.fr>
2016-01-20 10:25:53 +0100

    sprite and new style

--- chromoji/chrome/content.js
+++ chromoji/chrome/content.js
@@ -1,12 +1,13 @@
 // To implement
 url = chrome.extension.getURL("data/images/emoji/");
 emoji.img_path = url;
+emoji.use_sheet = true;
 
 emoji.img_sets = {
-    'apple': {'path': url + 'emoji-data/img-apple-64/', 'sheet': '/emoji-data/sheet_apple_64.png', 'mask': 1 },
-    'google': {'path': url + 'emoji-data/img-google-64/', 'sheet': '/emoji-data/sheet_google_64.png', 'mask': 2 },
-    'twitter': {'path': url + 'emoji-data/img-twitter-72/', 'sheet': '/emoji-data/sheet_twitter_64.png', 'mask': 4 },
-    'emojione': {'path': url + 'emoji-data/img-emojione-64/', 'sheet': '/emoji-data/sheet_emojione_64.png', 'mask': 8 }
+    'apple': {'path': url + 'emoji-data/img-apple-64/', 'sheet': url + 'emoji-data/sheet_apple_64.png', 'mask': 1 },
+    'google': {'path': url + 'emoji-data/img-google-64/', 'sheet': url + 'emoji-data/sheet_google_64.png', 'mask': 2 },
+    'twitter': {'path': url + 'emoji-data/img-twitter-72/', 'sheet': url + 'emoji-data/sheet_twitter_64.png', 'mask': 4 },
+    'emojione': {'path': url + 'emoji-data/img-emojione-64/', 'sheet': url + 'emoji-data/sheet_emojione_64.png', 'mask': 8 }
 };
 
 var storage = chrome.storage.local;
