07c8e60f2af3d2957ab231bbcead0760dc9bfb7f
Martin Smeeckaert <smeeckaert@novius.fr>
2016-02-09 17:43:27 +0100

    don't change editable sections

--- chromoji/chrome/content.js
+++ chromoji/chrome/content.js
@@ -4,10 +4,18 @@ emoji.img_path = url;
 emoji.use_sheet = true;
 
 emoji.img_sets = {
-    'apple': {'path': url + 'emoji-data/img-apple-64/', 'sheet': url + 'emoji-data/sheet_apple_64.png', 'mask': 1 },
-    'google': {'path': url + 'emoji-data/img-google-64/', 'sheet': url + 'emoji-data/sheet_google_64.png', 'mask': 2 },
-    'twitter': {'path': url + 'emoji-data/img-twitter-72/', 'sheet': url + 'emoji-data/sheet_twitter_64.png', 'mask': 4 },
-    'emojione': {'path': url + 'emoji-data/img-emojione-64/', 'sheet': url + 'emoji-data/sheet_emojione_64.png', 'mask': 8 }
+    'apple': {'path': url + 'emoji-data/img-apple-64/', 'sheet': url + 'emoji-data/sheet_apple_64.png', 'mask': 1},
+    'google': {'path': url + 'emoji-data/img-google-64/', 'sheet': url + 'emoji-data/sheet_google_64.png', 'mask': 2},
+    'twitter': {
+        'path': url + 'emoji-data/img-twitter-72/',
+        'sheet': url + 'emoji-data/sheet_twitter_64.png',
+        'mask': 4
+    },
+    'emojione': {
+        'path': url + 'emoji-data/img-emojione-64/',
+        'sheet': url + 'emoji-data/sheet_emojione_64.png',
+        'mask': 8
+    }
 };
 
 (function (window) {
@@ -135,6 +143,10 @@ function insert() {
             })
             .each(function () {
                 var $this = $(this);
+                var $parentEditable = $this.parents('[contenteditable="true"]');
+                if ($parentEditable.length) {
+                    return false;
+                }
                 var self = this;
                 content = emoji.replace_unified(self.textContent);
 
