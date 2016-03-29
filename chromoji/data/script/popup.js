6f968884ef0177f1f0c032bf38c58c2918fa07a5
Martin Smeeckaert <martin@smeeckaert.com>
2016-03-29 04:24:46 +0200

    update perfs

--- chromoji/data/script/popup.js
+++ chromoji/data/script/popup.js
@@ -2,25 +2,9 @@ BrowserAddOn.autoregister();
 
 BrowserAddOn.onload(function () {
 
-    listEmoji = emoji.data;
     $list = $(".emoji-list");
     $container = $(".emoji-list-container");
-    $input = $(".emoji-container-text")
-
-    setTimeout(function () {
-        for (i in listEmoji) {
-
-            currentEmo = listEmoji[i];
-            for (k in currentEmo[0]) {
-                $item = $('li:contains(' + currentEmo[0][k] + ')');
-                if ($item.length) {
-                    $item.attr('title', currentEmo[3][0]);
-                    $item.attr('data-name', currentEmo[3][0]);
-                }
-            }
-        }
-    }, 1000); // This process is greedy, so we put it in background. Could be improved if we put all the data into the dom but here it is.
-
+    $input = $(".emoji-container-text");
     $search = $(".search-field");
 
     $search.on('keyup change', function () {
@@ -55,67 +39,5 @@ BrowserAddOn.onload(function () {
         $input.val($input.val() + $this.find('.emoji-inner').html());
         $input.select();
     });
-
-    // Replace with content.js when we find out how it works
-
-
-    url = chrome.extension.getURL("data/images/emoji/");
-    emoji.img_path = url;
-    emoji.use_sheet = true;
-
-    emoji.img_sets = {
-        'apple': {'path': url + 'emoji-data/img-apple-64/', 'sheet': url + 'emoji-data/sheet_apple_64.png', 'mask': 1},
-        'google': {
-            'path': url + 'emoji-data/img-google-64/',
-            'sheet': url + 'emoji-data/sheet_google_64.png',
-            'mask': 2
-        },
-        'twitter': {
-            'path': url + 'emoji-data/img-twitter-72/',
-            'sheet': url + 'emoji-data/sheet_twitter_64.png',
-            'mask': 4
-        },
-        'emojione': {
-            'path': url + 'emoji-data/img-emojione-64/',
-            'sheet': url + 'emoji-data/sheet_emojione_64.png',
-            'mask': 8
-        }
-    };
-    var storage = chrome.storage.local;
-    storage.get('type', function (data) {
-
-        style = 'emojione';
-        if (data.type && data.type.style) {
-            style = data.type.style;
-        }
-        emoji.img_set = style;
-        $('*:not(iframe)')
-            .contents()
-            .filter(function () {
-                return this.nodeType === 3;
-            })
-            .each(function () {
-                var $this = $(this);
-                var self = this;
-                content = emoji.replace_unified(self.textContent);
-
-                if (content != this.textContent) {
-                    requestAnimationFrame(function () {
-                        $parent = $this.parent();
-                        fontSize = $parent.css('font-size');
-                        var replacementNode = document.createElement('span');
-                        replacementNode.className = 'emoji-container';
-                        replacementNode.innerHTML = emoji.replace_unified(self.textContent);
-                        self.parentNode.insertBefore(replacementNode, self);
-                        self.parentNode.removeChild(self);
-                        if (fontSize != '16px') {
-                            $parent.find('.emoji-sizer').css({width: fontSize, height: fontSize});
-                        }
-                    });
-                }
-
-            });
-    });
-
 
 });
