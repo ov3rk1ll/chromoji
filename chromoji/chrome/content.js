f40986ebcfde59cb5d8cb528111d24db70d21519
Martin Smeeckaert <martin@smeeckaert.com>
2016-04-15 18:26:48 +0200

    pevent replacing script and style

--- chromoji/chrome/content.js
+++ chromoji/chrome/content.js
@@ -61,7 +61,7 @@ function insert() {
         }
 
         emoji.img_set = style;
-        $('*:not(iframe):not(.emoji-inner)')
+        $('*:not(iframe):not(.emoji-inner):not(style):not(script)')
             .contents()
             .filter(function () {
                 return this.nodeType === 3;
